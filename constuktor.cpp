bce2cae369efda05b6c99bc5dfdc526516ac4c86
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-05-14 16:25:52 +0800

    bikin class mhs

---
+++ constuktor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+    public:
+    int nim;
+    string nama;
+    public:
+    mahasiswa()
+    {
+        nim = 0;
+        nama = "" ;
+    };
+
+    mahasiswa(int iNim)
+    {
+        nim = iNim;
+    }
+};
